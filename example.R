2cd7a2715311abd257958d91364969b4f192190b
Chris <42919372+Darachnid@users.noreply.github.com>
2019-10-03 15:56:25 -0400

    Add files via upload

---
+++ example.R
@@ -0,0 +1,30 @@
+#melt the 4 columns for rankings into 2
+data <- melt(data, 
+             id.vars = c("group", 
+                         "participant", 
+                         "time", 
+                         "scenario", 
+                         "subtheme", 
+                         "theme"),
+             measure.vars = c("swimming",
+                              "flooding",
+                              "river health",
+                              "water quality"))
+
+#index for spliting rows with themes or ranks
+themeNA <- which(is.na(data$theme) == TRUE)
+
+#define the split variable
+ranks <- data[themeNA,]
+themes <- data[-themeNA,]
+
+#plot the ranks
+plot <- ggplot(ranks, aes(x = time, 
+                         y = value, 
+                         color = participant, 
+                         group = participant)) +
+  geom_step() + 
+  facet_wrap(~variable, nrow = 4) +
+  theme_minimal()
+plot
+ 
